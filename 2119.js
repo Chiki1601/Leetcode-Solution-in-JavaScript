e181aa8517a5110f001a2f38b31d64e892a9771f
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-24 19:41:30 +0530

    Delete 2119.js

--- 2119.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} num
- * @return {boolean}
- */
-var isSameAfterReversals = function(num) {
-  if(('' +num).length === 1) return true
-  const tmp = (''+num).endsWith('0')
-  return !tmp
-};
