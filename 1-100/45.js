5378f8cd4e2de5500b96ae4119a10de0fb15cecd
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-29 12:08:56 +0530

    Create 45.js

---
+++ 1-100/45.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(N) {
+    let len = N.length - 1, curr = -1, next = 0, ans = 0
+    for (let i = 0; next < len; i++) {
+        if (i > curr) ans++, curr = next
+        next = Math.max(next, N[i] + i)
+    }
+    return ans
+};
