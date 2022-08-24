26652e97e09c6143205531a3b204a87764fe5368
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-24 11:01:25 +0530

    Create 112.js

---
+++ 101-500/112.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+    if (root.val === sum && !root.left && !root.right) return true;
+    if (hasPathSum(root.left, sum - root.val)) return true;
+    if (hasPathSum(root.right, sum - root.val)) return true;
+    return false;
+};
