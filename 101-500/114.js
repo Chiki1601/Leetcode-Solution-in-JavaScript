b9515ac75b1bba28a875df092f160d20477cf810
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-08-29 11:58:39 +0530

    Create 114.js

---
+++ 101-500/114.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root) {
+    if (!root) return;
+    // save right now
+    var tmp = root.right;
+    flatten(root.left);
+    flatten(root.right);
+    root.right = root.left;
+    root.left = null;
+    while (root.right) {
+        root = root.right;
+    }
+    root.right = tmp;
+};
