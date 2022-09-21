b65840e6fd2bddc2e18aff899e92c57c99e51df5
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-21 12:16:25 +0530

    Create 38.js

---
+++ 1-100/38.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {string}
+*/
+var countAndSay = function(n) {
+    var input = '1';
+    var result = input;
+    for (var i = 1; i < n; i++) {
+        result = '';
+        var count = 1;
+        for (var j = 0; j < input.length; j++) {
+            if (j === input.length - 1) {
+                result = result + count + input[j];
+                break;
+              }
+              if (input[j] === input[j+1]) {
+                  count++;
+              } else {
+                  result = result + count + input[j];
+                  count = 1;
+              }
+        }
+        input = result;
+    }
+    return result;
+};
