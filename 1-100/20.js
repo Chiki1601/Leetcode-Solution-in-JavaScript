6325432c41ac846a8caf7af5cb93b9399d2e00e4
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-21 12:11:32 +0530

    Create 20.js

---
+++ 1-100/20.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var stack = [];
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            stack.push(s[i]);
+        }
+
+        if (s[i] === ')') {
+            if (stack[stack.length - 1] !== '(') return false;
+            else stack.pop()
+        }
+
+        if (s[i] === ']') {
+            if (stack[stack.length - 1] !== '[') return false;
+            else stack.pop();
+        }
+
+        if (s[i] === '}') {
+            if (stack[stack.length - 1] !== '{') return false;
+            else stack.pop();
+        }
+    }
+
+    return stack.length === 0;
+};
