9788d970b57e0c37cd883e02a7f35fc9dcebc290
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-09-21 12:14:59 +0530

    Create 27.swift

---
+++ 1-100/27.swift
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var start = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[start] = nums[i];
+            start++;
+        }
+    }
+
+    return start;
+};
