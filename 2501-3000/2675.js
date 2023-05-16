25333aaff154880a0bac0b731cceb723e976deb7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-05-16 22:24:00 +0530

    Update and rename 2675.cpp to 2675.js

--- 2501-3000/2675.js
+++ 2501-3000/2675.js
@@ -3,7 +3,7 @@ const flatten = (a, prefix = []) => Object.entries(a)
     ? { ...r, [[...prefix, k].join('.')]: v }
     : { ...r, ...flatten(v, [...prefix, k]) }, {})
 
-onst jsonToMatrix = (a) => {
+const jsonToMatrix = (a) => {
   a = a.map(i => flatten(i))
   const keys = Object.keys(a.reduce((r, i) => ({ ...r, ...i }), {})).sort()
   return a.reduce((r, i) => [ ...r, keys.map(key => (key in i) ? i[key] : '')], [ keys ])
