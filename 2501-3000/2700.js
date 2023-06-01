3d00b61c73ae6f2610ceac3498876969e28c4a61
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-06-01 06:55:55 +0530

    Create 2700.js

---
+++ 2501-3000/2700.js
@@ -0,0 +1,46 @@
+/**
+ * @param {object} obj1
+ * @param {object} obj2
+ * @return {object}
+ */
+function objDiff(obj1, obj2) {
+  function isObject(val) {
+    return typeof val === 'object' && val !== null && !Array.isArray(val);
+  }
+
+  function compare(obj1, obj2, path, result) {
+    if (isObject(obj1) && isObject(obj2)) {
+      for (const key in obj1) {
+        if (key in obj2) {
+          compare(obj1[key], obj2[key], [...path, key], result);
+        }
+      }
+    } else if (Array.isArray(obj1) && Array.isArray(obj2)) {
+      const maxLength = Math.max(obj1.length, obj2.length);
+      for (let i = 0; i < maxLength; i++) {
+        if (i in obj1 && i in obj2) {
+          compare(obj1[i], obj2[i], [...path, i], result);
+        }
+      }
+    } else if (obj1 !== obj2) {
+      if (path.length === 0) {
+        result = [obj1, obj2];
+      } else {
+        let currentObj = result;
+        for (let i = 0; i < path.length - 1; i++) {
+          const key = path[i];
+          if (!(key in currentObj)) {
+            currentObj[key] = {};
+          }
+          currentObj = currentObj[key];
+        }
+        const lastKey = path[path.length - 1];
+        currentObj[lastKey] = [obj1, obj2];
+      }
+    }
+  }
+
+  const result = {};
+  compare(obj1, obj2, [], result);
+  return result;
+}
