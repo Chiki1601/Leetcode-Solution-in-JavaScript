6a3edb7a5cd08c06d52a577293d2b86b19332a5a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2024-02-22 10:42:01 +0530

    Create 997.js

---
+++ 501-1000/997.js
@@ -0,0 +1,12 @@
+var findJudge = function(N, trust) {
+  const array = new Array(N).fill(0);
+
+  trust.forEach(([a, b]) => {
+    array[a - 1]--;
+    array[b - 1]++;
+  });
+
+  const judge = array.indexOf(N - 1);
+
+  return judge !== -1 ? judge + 1 : -1;
+};
